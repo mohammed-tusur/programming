bf78532537d30cee40989bd84f499beacf450320
mohammed-tusur <cs.mohammedhammood@gmail.com>
2025-06-19 14:45:37 +0300

    removed .vs folder

--- src/Contacts/Contacts/Controls/ContactControl.xaml.cs
+++ src/Contacts/Contacts/Controls/ContactControl.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace View.Controls
 {
     /// <summary>
-    /// Логика взаимодействия для ContactControl.xaml
+    /// Interaction logic for ContactControl.xaml
     /// </summary>
     public partial class ContactControl : UserControl
     {
@@ -27,30 +17,33 @@ namespace View.Controls
         }
 
         /// <summary>
-        /// Валидирует вводимый текст в PhoneTextBox.
+        /// Validates text input in the PhoneTextBox.
         /// </summary>
         private void PhoneTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            string pattern = @"[^\d()+-]";
+            string pattern = @"[^\d()+-]";  // Regex pattern to block unwanted characters
             Regex regex = new Regex(pattern);
-            e.Handled = regex.IsMatch(e.Text);
+            e.Handled = regex.IsMatch(e.Text);  // Block input if invalid characters detected
         }
 
         /// <summary>
-        /// Валидирует вставку текста в PhoneTextBox.
+        /// Validates pasted text in the PhoneTextBox.
         /// </summary>
         private void PhoneTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
         {
             if (!e.DataObject.GetDataPresent(DataFormats.Text))
             {
-                e.CancelCommand();
+                e.CancelCommand();  // cancel if data isn't text
                 return;
             }
-            string pattern = @"[^\d()+-]";
+
+            string pattern = @"[^\d()+-]";  // same validation pattern as for direct input
             Regex regex = new Regex(pattern);
-            if (!regex.IsMatch((e.DataObject.GetData(DataFormats.Text)).ToString()))
+            string pastedText = e.DataObject.GetData(DataFormats.Text).ToString();
+
+            if (regex.IsMatch(pastedText))
             {
-                e.CancelCommand();
+                e.CancelCommand();  // cancel paste if invalid characters found
             }
         }
     }
